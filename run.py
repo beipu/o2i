68afe53e9bc92887daf443d2ccac538498d2a4af
beipu <73775143+beipu@users.noreply.github.com>
2025-07-30 12:51:54 +0800

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-import os
-import uvicorn
-from src.main import app
-
-if __name__ == "__main__":
-    host = os.getenv("HOST", "0.0.0.0")
-    port = int(os.getenv("PORT", "8888"))
-    uvicorn.run(app, host=host, port=port)
